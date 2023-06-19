6593b613b9785a52a1241b0d6bc62b6baaf985d1
gabriellsbraz <gabriel_lsb@hotmail.com>
2023-06-18 22:56:16 -0300

    [M1S04] Ex 1 - Média com JS

--- exercicio1.js
+++ exercicio1.js
@@ -3,4 +3,5 @@ let n2 = parseFloat(prompt("Digite o segundo numero: "));
 let n3 = parseFloat(prompt("Digite o terceiro numero: "));
 
 let media = ((n1 + n2 + n3) / 3)
+
 console.log("A soma de " + n1 + " " + n2 + " " + n3 + " resulta na media: " + media)
