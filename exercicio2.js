674bddc3a9e5f788a0a93803448318827ad2b73b
gabriellsbraz <gabriel_lsb@hotmail.com>
2023-06-18 22:51:16 -0300

    [M1S04] Ex 2 - Calculadora de IMC

---
+++ exercicio2.js
@@ -0,0 +1,10 @@
+function calcIMC(peso, altura) {
+  let imc = (peso/(altura*altura))
+  return imc.toFixed(1);
+}
+
+let peso = 79.6;
+let altura = 1.79;
+
+let resultIMC = calcIMC(peso, altura)
+console.log(resultIMC)
